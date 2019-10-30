fde894bc7d60463cd77503b18dc5afb4a317916f
Bassel Al Araaj <info@basselalaraaj.com>
2019-10-30 20:07:52 +0100

    First and Last Indices of an Element in a Sorted Array

---
+++ first_and_last_indices_of_an_element_in_a_sorted_array.py
@@ -0,0 +1,33 @@
+Given a sorted array, A, with possibly duplicated elements, find the indices of the first and last occurrences of a target element, x. Return -1 if the target is not found.
+
+Example:
+Input: A = [1,3,3,5,7,8,9,9,9,15], target = 9
+Output: [6,8]
+
+Input: A = [100, 150, 150, 153], target = 150
+Output: [1,2]
+
+Input: A = [1,2,3,4,5,6,10], target = 9
+Output: [-1, -1]
+
+Solution:
+class Solution: 
+  def getRange(self, arr, target):
+    foundStart = -1
+    foundEnd = -1
+
+    for i in range(len(arr)):
+      if arr[i] == target:
+        if foundStart == -1:
+          foundStart = i
+        foundEnd = i
+    
+    return [foundStart,foundEnd]
+
+  
+# Test program 
+arr = [1, 2, 2, 2, 2, 3, 4, 7, 8, 8] 
+x = 2
+print(Solution().getRange(arr, x))
+# [1, 4]
+print(Solution().getRange(arr, 9))
